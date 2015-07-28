46bb2d11d1ee7b154e5124adcbe43416591e8fff
Jack Firth <jackhfirth@gmail.com>
2015-07-28 12:29:06 -0700

    Add package setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(name='pamda',
+      version='0.1',
+      description='A Python package for curried functional programming',
+      url='http://github.com/jackfirth/pamda',
+      author='Jack Firth',
+      author_email='jackhfirth@gmail.com',
+      packages=['pamda'],
+      zip_safe=False)
