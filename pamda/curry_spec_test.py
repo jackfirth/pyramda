fe15b107468868159e3f8b65dce2cf22eb745f53
Jack Firth <jackhfirth@gmail.com>
2015-07-28 12:25:33 -0700

    Add args_overlap test

--- pamda/curry_spec_test.py
+++ pamda/curry_spec_test.py
@@ -7,3 +7,7 @@ def curry_spec_creation_test():
 
 def num_args_test():
     assert num_args(ArgValues([1, 2], {'foo': 5, 'bar': 7, 'baz': 10})) == 3
+
+
+def args_overlap_test():
+    assert args_overlap(ArgValues([], {'foo': 1}), ArgValues([], {'foo': 10}))
