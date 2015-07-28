7d7febd1743fa5ee7a16b43d4ab1cf6e26de9f78
Jack Firth <jackhfirth@gmail.com>
2015-07-28 14:46:46 -0700

    Separate out spec fulfillment relation

---
+++ pamda/curry_spec_fulfill_test.py
@@ -0,0 +1,31 @@
+from functools import partial, wraps
+from .curry_spec_fulfill import *
+from .curry_spec import make_func_curry_spec, ArgValues
+from .private.assert_pred import assert_pred, true_case, false_case
+from .private.assert_domain import assert_not_in_domain
+
+
+def f(x, y, z=3):
+    return x + y + z
+
+
+def arg_values_fulfill_curry_spec_test():
+    f_spec = make_func_curry_spec(f)
+    arg_values_fulfill_f_spec = wraps(arg_values_fulfill_curry_spec)(
+        partial(arg_values_fulfill_curry_spec, f_spec)
+    )
+    assert_pred(
+        arg_values_fulfill_f_spec,
+        true_case(ArgValues([1, 2, 3], {})),
+        true_case(ArgValues([], {'x': 1, 'y': 2, 'z': 3})),
+        true_case(ArgValues([1, 2], {})),
+        true_case(ArgValues([], {'x': 1, 'y': 2})),
+        true_case(ArgValues([1], {'y': 5})),
+        false_case(ArgValues([1], {})),
+        false_case(ArgValues([], {'x': 10})),
+        false_case(ArgValues([1], {'z': 10}))
+    )
+    assert_not_in_domain(
+        arg_values_fulfill_f_spec,
+        ArgValues([1], {'x': 1})
+    )
--- pamda/curry_spec_test.py
+++ pamda/curry_spec_test.py
@@ -27,25 +27,3 @@ def f(x, y, z=3):
 def make_func_curry_spec_test():
     f_spec = make_func_curry_spec(f)
     assert f_spec == CurrySpec(['x', 'y', 'z'], {'z': 3})
-
-
-def arg_values_fulfill_curry_spec_test():
-    f_spec = make_func_curry_spec(f)
-    arg_values_fulfill_f_spec = wraps(arg_values_fulfill_curry_spec)(
-        partial(arg_values_fulfill_curry_spec, f_spec)
-    )
-    assert_pred(
-        arg_values_fulfill_f_spec,
-        true_case(ArgValues([1, 2, 3], {})),
-        true_case(ArgValues([], {'x': 1, 'y': 2, 'z': 3})),
-        true_case(ArgValues([1, 2], {})),
-        true_case(ArgValues([], {'x': 1, 'y': 2})),
-        true_case(ArgValues([1], {'y': 5})),
-        false_case(ArgValues([1], {})),
-        false_case(ArgValues([], {'x': 10})),
-        false_case(ArgValues([1], {'z': 10}))
-    )
-    assert_not_in_domain(
-        arg_values_fulfill_f_spec,
-        ArgValues([1], {'x': 1})
-    )
