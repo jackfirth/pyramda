d3c9e2ae824e3edda9c68ad0815c3ee4ff84de2d
Jack Firth <jackhfirth@gmail.com>
2015-07-28 13:59:16 -0700

    Add curry spec fulfillment relation

--- pamda/curry_spec.py
+++ pamda/curry_spec.py
@@ -8,13 +8,25 @@ ArgValues = namedtuple('ArgValues', 'args kwargs')
 class CurrySpecVarargError(ValueError):
     def __init__(self, f):
         name = f.__name__
-        message_template = "Cannot curry vararg or varkwarg function {0}"
+        message_template = "Cannot curry var-arg or var-kwarg function {0}"
         message = message_template.format(name)
         super(CurrySpecVarargError, self).__init__(message)
 
 
 def num_args(arg_values):
     return len(arg_values.args) + len(arg_values.kwargs)
+
+
+def num_curry_spec_args(curry_spec):
+    return len(curry_spec.arg_names)
+
+
+def num_curry_spec_default_args(curry_spec):
+    return len(curry_spec.arg_defaults)
+
+
+def num_default_overriding_args(curry_spec, arg_values):
+    return len(curry_spec.arg_defaults.keys() & arg_values.kwargs.keys())
 
 
 def args_overlap(arg_values1, arg_values2):
@@ -50,3 +62,26 @@ def make_func_curry_spec(f):
     if func_accepts_varargs(f):
         raise CurrySpecVarargError(f)
     return CurrySpec(func_arg_names(f), func_arg_defaults(f))
+
+
+def has_extra_kwargs(curry_spec, kwargs):
+    return len(kwargs.keys() - curry_spec.arg_names) > 0
+
+
+def arg_values_fulfill_curry_spec(curry_spec, arg_values):
+    if has_extra_kwargs(curry_spec, arg_values.kwargs):
+        return False
+
+    n_args = num_args(arg_values)
+    n_curry_args = num_curry_spec_args(curry_spec)
+    n_curry_default_args = num_curry_spec_default_args(curry_spec)
+    n_default_overriding_args = num_default_overriding_args(
+        curry_spec,
+        arg_values
+    )
+
+    n_min_curry_args = n_curry_args - n_curry_default_args
+
+    return (
+        n_min_curry_args <= n_args - n_default_overriding_args <= n_curry_args
+    )
--- pamda/curry_spec_test.py
+++ pamda/curry_spec_test.py
@@ -1,3 +1,4 @@
+from functools import partial, wraps
 from .curry_spec import *
 
 
@@ -17,8 +18,64 @@ def args_overlap_test():
     assert args_overlap(ArgValues([], {'foo': 1}), ArgValues([], {'foo': 10}))
 
 
+def f(x, y, z=3):
+    return x + y + z
+
+
 def make_func_curry_spec_test():
-    def f(x, y, z=3):
-        return x + y + z
     f_spec = make_func_curry_spec(f)
     assert f_spec == CurrySpec(['x', 'y', 'z'], {'z': 3})
+
+
+def true_case(v):
+    return (v, True)
+
+
+def false_case(v):
+    return (v, False)
+
+
+class PredicateCaseAssertionError(AssertionError):
+    def __init__(self, expected, pred, v):
+        name = pred.__name__
+        v_str = str(v)
+        message_template = "Expected predicate {0} to be {1} for input {2}"
+        message = message_template.format(name, expected, v_str)
+        super(PredicateCaseAssertionError, self).__init__(message)
+
+
+class PredicateFalseCaseAssertionError(PredicateCaseAssertionError):
+    def __init__(self, pred, v):
+        super(PredicateFalseCaseAssertionError, self).__init__(False, pred, v)
+
+
+class PredicateTrueCaseAssertionError(PredicateCaseAssertionError):
+    def __init__(self, pred, v):
+        super(PredicateTrueCaseAssertionError, self).__init__(True, pred, v)
+
+
+def assert_pred(pred, *cases):
+    for v, expected in cases:
+        pv = pred(v)
+        if pv and not expected:
+            raise PredicateFalseCaseAssertionError(pred, v)
+        if not pv and expected:
+            raise PredicateTrueCaseAssertionError(pred, v)
+
+
+def arg_values_fulfill_curry_spec_test():
+    f_spec = make_func_curry_spec(f)
+    arg_values_fulfill_f_spec = wraps(arg_values_fulfill_curry_spec)(
+        partial(arg_values_fulfill_curry_spec, f_spec)
+    )
+    assert_pred(
+        arg_values_fulfill_f_spec,
+        true_case(ArgValues([1, 2, 3], {})),
+        true_case(ArgValues([], {'x': 1, 'y': 2, 'z': 3})),
+        true_case(ArgValues([1, 2], {})),
+        true_case(ArgValues([], {'x': 1, 'y': 2})),
+        true_case(ArgValues([1], {'y': 5})),
+        false_case(ArgValues([1], {})),
+        false_case(ArgValues([], {'x': 10})),
+        false_case(ArgValues([1], {'z': 10}))
+    )
