08eb7aac392ca3492d2d3c77112c7fbd2f41571a
Jack Firth <jackhfirth@gmail.com>
2015-07-28 12:24:22 -0700

    Add num_args test

--- pamda/curry_spec_test.py
+++ pamda/curry_spec_test.py
@@ -3,3 +3,7 @@ from curry_spec import *
 
 def curry_spec_creation_test():
     assert CurrySpec(['x', 'y', 'z'] {'y': 5}) isinstance CurrySpec
+
+
+def num_args_test():
+    assert num_args(ArgValues([1, 2], {'foo': 5, 'bar': 7, 'baz': 10})) == 3
