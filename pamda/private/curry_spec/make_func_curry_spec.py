7bc7bfa84550b4037672cc1168a178cf20f0c548
Jack Firth <jackhfirth@gmail.com>
2015-07-29 15:09:27 -0700

    Fix make_fun_curry_spec for functions with no defaults

--- pamda/private/curry_spec/make_func_curry_spec.py
+++ pamda/private/curry_spec/make_func_curry_spec.py
@@ -18,7 +18,7 @@ def func_arg_names(f):
 def func_arg_defaults(f):
     argspec = getargspec(f)
     arg_names = argspec.args
-    default_arg_values = argspec.defaults
+    default_arg_values = argspec.defaults or []
     num_defaults = len(default_arg_values)
     default_arg_names = arg_names[-num_defaults:]
     return dict(zip(default_arg_names, default_arg_values))
