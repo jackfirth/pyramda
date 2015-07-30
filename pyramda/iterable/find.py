3e82c7ae1b76b4d5adbe4feb4e792813fd9e0fc5
Jack Firth <jackhfirth@gmail.com>
2015-07-30 16:29:22 -0700

    Make find stricter

--- pyramda/iterable/find.py
+++ pyramda/iterable/find.py
@@ -5,3 +5,7 @@ from pyramda.function.curry import curry
 def find(p, xs):
     for found in (x for x in xs if p(x)):
         return found
+    raise ValueError(
+        "Expected a list such that at least one item would satisfy " +
+        "{0}, got {1}".format(p, xs)
+    )
