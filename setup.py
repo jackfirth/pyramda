a6e1d99d8870e1b24de38b534220842a58155784
Jack Firth <jackhfirth@gmail.com>
2015-07-30 11:24:02 -0700

    Rename to pyramda

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 
-setup(name='pamda',
+setup(name='pyramda',
       version='0.1',
       description='A Python package for curried functional programming',
-      url='http://github.com/jackfirth/pamda',
+      url='http://github.com/jackfirth/pyramda',
       author='Jack Firth',
       author_email='jackhfirth@gmail.com',
-      packages=['pamda'],
+      packages=['pyramda'],
       zip_safe=False)
