4426ffe69794b7dfc394bd4a5f5fcdaaec01f140
Jack Firth <jackhfirth@gmail.com>
2015-08-03 13:06:17 -0700

    Fix packages declaration

--- setup.py
+++ setup.py
@@ -1,12 +1,24 @@
 from setuptools import setup
 
 
-setup(name='pyramda',
-      version='0.1',
-      description='A Python package for curried functional programming',
-      url='http://github.com/jackfirth/pyramda',
-      author='Jack Firth',
-      author_email='jackhfirth@gmail.com',
-      packages=['pyramda'],
-      tests_require=['nose', 'coverage'],
-      zip_safe=False)
+setup(
+    name='pyramda',
+    version='0.1',
+    description='A Python package for curried functional programming',
+    url='http://github.com/jackfirth/pyramda',
+    author='Jack Firth',
+    author_email='jackhfirth@gmail.com',
+    packages=[
+        'pyramda',
+        'pyramda.dictionary',
+        'pyramda.function',
+        'pyramda.iterable',
+        'pyramda.logic',
+        'pyramda.math',
+        'pyramda.private',
+        'pyramda.private.curry_spec',
+        'pyramda.relation'
+    ],
+    tests_require=['nose', 'coverage'],
+    zip_safe=False
+)
