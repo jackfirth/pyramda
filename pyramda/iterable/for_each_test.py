803bed5400d341a7c282b4736d59c59e1b9c7eea
sabbas <syed.k.abbas1988@gmailcom>
2017-07-15 12:31:53 -0500

    Fix inconsistent function naming

--- pyramda/iterable/for_each_test.py
+++ pyramda/iterable/for_each_test.py
@@ -16,7 +16,7 @@ def test_for_each_curry_returns_the_original_iterable():
     assert for_each(mock.MagicMock())([1, 2, 3]) == [1, 2, 3]
 
 
-def test_for_each_no_curry_executed_function_for_each_item_in_the_iterable():
+def test_for_each_nocurry_executed_function_for_each_item_in_the_iterable():
     m = mock.MagicMock()
     for_each(m, ([1, 2, 3])) == [1, 2, 3]
     assert len(m.mock_calls) == 3
