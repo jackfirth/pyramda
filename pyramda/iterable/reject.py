bc82fd7966f2c383babca57a3eebfd1a90bb616e
sabbas <syed.k.abbas1988@gmailcom>
2017-07-17 07:02:11 -0500

    Update docstring

--- pyramda/iterable/reject.py
+++ pyramda/iterable/reject.py
@@ -8,7 +8,7 @@ def reject(f, xs):
     Acts as a compliment of `filter`
 
     :param f: function
-    :param xs: Iterable. List or tuple or set. Note that a dict is not yet supported
+    :param xs: Iterable. A sequence, a container which supports iteration or an iterator
     :return: list
     """
     return list(set(xs) - set(filter(f, xs)))
