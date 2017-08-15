c1e10838ceea7eca6c0667353d29afd0bc15b58d
sabbas <syed.k.abbas1988@gmail.com>
2017-08-14 19:42:19 -0500

    Move flip_first_two func def outside of flip

--- pyramda/function/flip.py
+++ pyramda/function/flip.py
@@ -1,6 +1,6 @@
 from .curry import (
     curry,
-    curry_by_spec
+    curry_by_spec,
 )
 
 from ..private.curry_spec import make_func_curry_spec
@@ -13,12 +13,13 @@ def flip(f):
     arguments
     """
 
-    def flip_first_two(xs):
-        return xs[1::-1] + xs[2::] if len(xs) > 1 else xs
-
     def wrapped(*args, **kwargs):
         return f(*flip_first_two(args), **kwargs)
 
     f_spec = make_func_curry_spec(f)
 
     return curry_by_spec(f_spec, wrapped)
+
+
+def flip_first_two(xs):
+    return xs[1::-1] + xs[2::] if len(xs) > 1 else xs
