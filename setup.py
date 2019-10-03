f5b4585cc21a7f8d8101691b6bc07e74b08dce20
Vijaybabu Gangaprasad <vijay.gangaprasad@outlook.com>
2019-10-03 00:48:27 -0700

    Updated Setup.py to remove Pasteurize
    Remove the following logic as Python 2 is being retired globally starting in 2020
    
    from setuptools.command.build_py import build_py
                              : 
                              : 
    class PasteurizeBuildCommand(build_py):
        def run(self):
            call(["pip", "install", "future"])
            call(["pasteurize", "./pyramda"])
            build_py.run(self)
                              : 
                              : 
    cmdclass={
            'build_py': PasteurizeBuildCommand
        },

--- setup.py
+++ setup.py
@@ -1,14 +1,5 @@
 from setuptools import setup
-from setuptools.command.build_py import build_py
 from subprocess import call
-
-
-class PasteurizeBuildCommand(build_py):
-    def run(self):
-        call(["pip", "install", "future"])
-        call(["pasteurize", "./pyramda"])
-        build_py.run(self)
-
 
 setup(
     name='pyramda',
@@ -30,8 +21,5 @@ setup(
     ],
     install_requires=['future'],
     tests_require=['nose', 'coverage', 'mock'],
-    cmdclass={
-        'build_py': PasteurizeBuildCommand
-    },
     zip_safe=False
 )
