7e350b1437a244feea989afb5af9f10c6bc6a045
Vijaybabu Gangaprasad <vijay.gangaprasad@outlook.com>
2019-10-04 07:17:34 -0700

    Updated to import basestring from builtins
    from builtins import basestring

--- pyramda/iterable/flatten.py
+++ pyramda/iterable/flatten.py
@@ -1,6 +1,7 @@
 from collections import Iterable
 from pyramda.isinstance import isinstance
 from pyramda.function.curry import curry
+from builtins import basestring
 
 
 @curry
@@ -25,4 +26,4 @@ def flatten_until(is_leaf, xs):
 
     return list(_flatten_until(xs))
 
-flatten = flatten_until(isinstance(string_types))
+flatten = flatten_until(isinstance(basestring))
