7a657f11365beef41e7932e625b72331a0d19cc7
Vijaybabu Gangaprasad <vijay.gangaprasad@outlook.com>
2019-10-04 13:53:57 -0700

    Update flatten.py

--- pyramda/iterable/flatten.py
+++ pyramda/iterable/flatten.py
@@ -1,7 +1,7 @@
 from collections import Iterable
 from pyramda.isinstance import isinstance
 from pyramda.function.curry import curry
-from builtins import basestring
+from builtins import str
 
 
 @curry
@@ -26,4 +26,4 @@ def flatten_until(is_leaf, xs):
 
     return list(_flatten_until(xs))
 
-flatten = flatten_until(isinstance(basestring))
+flatten = flatten_until(isinstance(str))
