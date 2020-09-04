eaae4b81d6c3f3b42ad9395d9d5d724a55352fba
Sérgio Rafael Siqueira <osergiosiqueira@gmail.com>
2020-09-04 11:37:56 -0300

    Improve pick

--- pyramda/dictionary/pick.py
+++ pyramda/dictionary/pick.py
@@ -3,8 +3,4 @@ from pyramda.function.curry import curry
 
 @curry
 def pick(keys, dict):
-    picked_dict = {}
-    for k in keys:
-        if k in dict:
-            picked_dict[k] = dict[k]
-    return picked_dict
+    return {k: v for k, v in dict.items() if k in keys}
